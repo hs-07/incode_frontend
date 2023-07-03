c381973ca87abe24a2ab89e769a80536b09b9877
Harshit Shukla <harshitcr7777@gmail.com>
2023-07-04 01:20:33 +0530

    Added images & icons, set background & home structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const index = () => {
+const index: React.FC = () => {
   return (
-    <div>index</div>
-  )
-}
+    <div className="flex w-screen justify-end">
+      <div className="sm:w-full md:w-1/2 lg:w-1/3 h-screen bg-white">
+        <h1>Hello</h1>
+      </div>
+    </div>
+  );
+};
 
-export default index
+export default index;
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,11 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens: {
+      sm: "300px",
+      md: "768px",
+      lg: "1024px",
+    },
   },
   plugins: [],
 }
