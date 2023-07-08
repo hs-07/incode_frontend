2a290458008848935959cc71c4a54ac0d337b039
Harshit Shukla <harshitcr7777@gmail.com>
2023-07-09 01:33:42 +0530

    Trip history API Integration done

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,6 +1,15 @@
-import React from "react";
-import { Navbar } from "@/components/common"; 
-import { Tab, Requests,Labels } from "@/components/helpers";
+import React, { useEffect, useState } from "react";
+import { Navbar } from "@/components/common";
+import axios from "axios";
+import { Tab, Requests, Labels } from "@/components/helpers";
+
+interface RequestProps {
+  destinationAddress: string;
+  startTime: string;
+  price: string;
+}
+
+const API = "https://incode-backend.onrender.com/api/trips";
 
 const historyConst = {
   Request: "Request History",
@@ -8,20 +17,31 @@ const historyConst = {
   Past: "Past Requests",
 };
 
-const requests = [
-{name:'JSSATE, Noida', date: '1 July', price:'₹300.00', link:'/details'},
-{name:'JSSATE, Noida', date: '1 July', price:'₹320.00', link:'/details'},
-{name:'JSSATE, Noida', date: '1 July', price:'₹300.00', link:'/details'},
-]
-const labels =[
-{src:'/assets/icons/Clock.svg', name: 'View older requests', url: ''},
-{src:'/assets/icons/heart.svg', name: 'View saved drivers', url: '/savedDrivers'}
-]
+const labels = [
+  { src: '/assets/icons/Clock.svg', name: 'View older requests', url: '' },
+  { src: '/assets/icons/heart.svg', name: 'View saved drivers', url: '/savedDrivers' }
+];
 
-const history: React.FC = () => {
+const History: React.FC<RequestProps> = ({ destinationAddress, startTime, price }) => {
+  const [trips, setTrips] = useState<RequestProps[]>([ ]);
+
+  const getTrips = async (url: string) => {
+    try {
+      const res = await axios.get<{ trips: RequestProps[] }>(url);
+      setTrips(res.data.trips);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTrips(`${API}/getCurrentUserTrips?userId=64a3f3a98424bf3d4b637c26`);
+  }, []);
+
   return (
     <div className="flex justify-end w-screen h-screen">
-      <div className="flex sm:w-full md:w-1/2 lg:w-1/3 h-screen bg-white overflow-y-scroll scroll-smooth scrollbar-thin  scrollbar-track-rounded-full  scrollbar-thumb-rounded-full scrollbar-thumb-gray-300">
+      <div className="flex sm:w-full md:w-1/2 lg:w-1/3 h-screen bg-white ">
         <div className="flex w-full flex-col">
           <Navbar className="bg-white" />
           <div className="flex flex-col px-5 pb-2 ">
@@ -32,18 +52,29 @@ const history: React.FC = () => {
             <Tab />
           </div>
           {/* Requests starts */}
-          <div className="flex flex-col px-5">
+          <div className="flex flex-col px-5 overflow-y-scroll scroll-smooth scrollbar-thin  scrollbar-track-rounded-full  scrollbar-thumb-rounded-full scrollbar-thumb-gray-300">
             <h1 className="">{historyConst.Past}</h1>
-            {requests.map((request) => (
-              <a href={request.link}>
-                <Requests name={request.name} date={request.date} price={request.price}/>
+            {
+              trips.map((trip, index) => (
+                <a href="/details" key={index}>
+                  <Requests
+                    name={trip.destinationAddress}
+                    date={trip.startTime}
+                    price={trip.price}
+                  />
                 </a>
-            ))}
+              ))}
           </div>
           {/* Requests ends */}
           <div className="flex flex-col lg:py-4 sm:py-5 px-5 gap-2">
-            {labels.map((label) => (
-                <Labels name={label.name} url={label.url} className=" w-10 " src={label.src}/>
+            {labels.map((label, index) => (
+              <Labels
+                key={index}
+                name={label.name}
+                url={label.url}
+                className="w-10"
+                src={label.src}
+              />
             ))}
           </div>
         </div>
@@ -52,4 +83,4 @@ const history: React.FC = () => {
   );
 };
 
-export default history;
+export default History;
